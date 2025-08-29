ed5e38d6bdd19b0682349ce659f2fe5d655393ec
Riko Mia <rikomia722@gmail.com>
2025-08-29 14:52:12 +0600

    History Section done

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const historyesSection = document.getElementById("historyes")
 const copyNum = document.getElementById("copy-text")
 const hartText = document.getElementById("hart-text")
 const hartClass = document.getElementsByClassName("fa-heart")
-let historyes = []
+// let historyes = []
 let duplicates =[]
 let copyCount = 0
 let heartCountNum = 0
@@ -63,16 +63,30 @@ function callFunction(e){
         if(num > 0){
 
             coins.innerText = num - 20
-            let history = {
-                title: item.parentNode.children[0].innerText,
-                number: item.parentNode.children[2].innerText,
-                date: new Date().toLocaleTimeString()
-            }
+            // let history = {
+            //     title: item.parentNode.children[0].innerText,
+            //     number: item.parentNode.children[2].innerText,
+            //     date: new Date().toLocaleTimeString()
+            // }
 
-            historyes.push(history)
+            // historyes.push(history)
 
             alert(`Calling ${item.parentNode.children[0].innerText}  ${item.parentNode.children[2].innerText}...`)
-                getHistory()
+                // getHistory()
+
+
+
+                const newCart = document.createElement('div')
+                
+                newCart.innerHTML =`<div id="historyes" class="flex justify-between items-center p-5 bg-[#FAFAFA] rounded-xl mb-4">
+                    <div>
+                        <p class="text-[18px]">${item.parentNode.children[0].innerText} </p>
+                        <p class="text-[18px] text-[#5C5C5C]">${item.parentNode.children[2].innerText}</p>
+                    </div>
+                    <p class="text-[18px]">${new Date().toLocaleTimeString()}</p>
+                </div>`
+
+                historyesSection.appendChild(newCart)
             
 
         } else{
@@ -80,7 +94,7 @@ function callFunction(e){
             alert('need coin')
         }
 
-console.log(historyes, '==============')
+// console.log(historyesSection, '==============')
 
     }
     }
@@ -88,58 +102,73 @@ console.log(historyes, '==============')
 
 // for (let h of historyes){
 
-//                 const p = document.createElement("p")
-//                 const p2 = document.createElement("p")
-//                 const p3 = document.createElement("p")
-//                 const createDiv = document.createElement("div")
-//                 createDiv.classList.add("historyes")
-//                 const createInDiv = document.createElement("div")
-//                 let createNewDiv = createDiv.appendChild(createInDiv) 
+                // const p = document.createElement("p")
+                // const p2 = document.createElement("p")
+                // const p3 = document.createElement("p")
+                // const createDiv = document.createElement("div")
+                // createDiv.classList.add("historyes")
+                // const createInDiv = document.createElement("div")
+                // let createNewDiv = createDiv.appendChild(createInDiv) 
 
-//                 let para = createNewDiv.appendChild(p) 
-//                 para.innerText = h.title
-//                 let para2 = createNewDiv.appendChild(p2) 
-//                 para2.innerText = h.number
-//                 let para3 = createDiv.appendChild(p3) 
-//                 para3.innerText = h.date
+                // let para = createNewDiv.appendChild(p) 
+                // para.innerText = h.title
+                // let para2 = createNewDiv.appendChild(p2) 
+                // para2.innerText = h.number
+                // let para3 = createDiv.appendChild(p3) 
+                // para3.innerText = h.date
 
+
+                // const newCart = document.createElement('div')
                 
-//                 historyesSection.appendChild(createDiv)
-//             }
+                // newCart.innerHTML =`<div id="historyes" class="flex justify-between items-center p-5 bg-[#FAFAFA] rounded-xl mb-4">
+                //     <div>
+                //         <p class="text-[18px]">${h.title} </p>
+                //         <p class="text-[18px] text-[#5C5C5C]">${h.number}</p>
+                //     </div>
+                //     <p class="text-[18px]">${h.date}</p>
+                // </div>`
+
+                // historyesSection.appendChild(newCart)
+            // }
 
  } 
 
 
 
-function getHistory(){
+// function getHistory(){
 
-    for (let h of historyes){
-        
+    // for (let i of historyes){
+        // for(let i = 0; i< historyes.length; i++){
+            // historyesSection.removeChild(historyesSection.firstChild)
+                // if(historyesSection.children.length >1){
+                //     console.log(historyesSection.removeChild(historyesSection.firstChild))
+                // } else {
+                //     // break
+                // }
+                // const p = document.createElement("p")
+                // const p2 = document.createElement("p")
+                // const p3 = document.createElement("p")
+                // const createDiv = document.createElement("div")
+                // createDiv.classList.add("historyes")
+                // const createInDiv = document.createElement("div")
+                // let createNewDiv = createDiv.appendChild(createInDiv) 
 
-                const p = document.createElement("p")
-                const p2 = document.createElement("p")
-                const p3 = document.createElement("p")
-                const createDiv = document.createElement("div")
-                createDiv.classList.add("historyes")
-                const createInDiv = document.createElement("div")
-                let createNewDiv = createDiv.appendChild(createInDiv) 
-
-                let para = createNewDiv.appendChild(p) 
-                para.innerText = h.title
-                let para2 = createNewDiv.appendChild(p2) 
-                para2.innerText = h.number
-                let para3 = createDiv.appendChild(p3) 
-                para3.innerText = h.date
+                // let para = createNewDiv.appendChild(p) 
+                // para.innerText = i.title
+                // let para2 = createNewDiv.appendChild(p2) 
+                // para2.innerText = i.number
+                // let para3 = createDiv.appendChild(p3) 
+                // para3.innerText = i.date
 
                                                         
-                historyesSection.appendChild(createDiv)
+                // historyesSection.appendChild(createDiv)
 
 
 
-console.log(h)
+// console.log(i)
 
-            }
-}
+//             }
+// }
 
 // console.log(historyes)
 
